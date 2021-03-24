659e6c69ea3cd6757dd2b257444cfae03f8493f0
Alexander Yankovskiy-Betcher <ayb@agritask.com>
2021-03-24 17:18:57 +0200

    Added DEBUG var and timeout handling

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Another mini-tool for sending messages to Slack or another compatible service fr
 
 ## Requirements
 System variables:
-* SLACK_API_URL — e.g. https://hooks.slack.com/services/xxxxxxxx/xxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxxx
+* API_URL — e.g. https://hooks.slack.com/services/xxxxxxxx/xxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxxx
 * MESSAGE — e.g. {"channel": "#devops", "username": "test", "text": "This is a test."}
 
 ## How to use
 
-`docker run --rm -e MESSAGE=$MESSAGE -e SLACK_API_URL=$SLACK_API_URL slacker`
+`docker run --rm -e MESSAGE=$MESSAGE -e API_URL=$API_URL suncheez/slacker`
 
 ## Why
 
 Because curl isn't available everywhere and just for fun.
-This tool replaces a command: `curl -s -X POST --data-urlencode "${MESSAGE}" ${SLACK_API_URL}`
+This tool replaces a command: `curl -s -X POST --data-urlencode "${MESSAGE}" ${API_URL}`
--- main.go
+++ main.go
@@ -6,36 +6,50 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
-func main() {
-	getEnv := func(key string) {
-		_, ok := os.LookupEnv(key)
-		if !ok {
-			fmt.Printf("[ FATAL ] %s not set."+
-				"[ FATAL ] SLACK_API_URL and MESSAGE env vars must be defined!"+
-				"[ FATAL ] MESSAGE shall be a json like {\"channel\": \"#devops\", \"username\": \"test\", \"text\": \"This is another test.\"}\n", key)
-			os.Exit(1)
-		} else {
-			fmt.Printf("%s found\n", key)
-		}
+func fatalExit (key string) {
+	fmt.Printf("[ FATAL ] %s not set.\n" +
+		"          Both API_URL and MESSAGE env vars must be defined!\n" +
+		"          MESSAGE shall be a json like {\"channel\": \"devops\", \"username\": \"test\", \"text\": \"This is another test.\"}\n", key)
+	os.Exit(1)
+}
+
+func logger (key string, val string) {
+	if len(os.Getenv("DEBUG")) != 0 {
+		fmt.Printf("[ DEBUG ] %s found\n          Value: %s\n", key, val)
 	}
-	getEnv("SLACK_API_URL")
+}
+
+func getEnv (key string) {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		fatalExit(key)
+	} else {
+		if len(val) == 0 {
+			fatalExit(key)
+		}
+		logger(key, val)
+	}
+}
+
+func main() {
+	getEnv("API_URL")
 	getEnv("MESSAGE")
-	var url = os.Getenv("SLACK_API_URL")
+	var url = os.Getenv("API_URL")
 	var jsonStr = []byte(os.Getenv("MESSAGE"))
-	// var jsonValue, _ = json.Marshal(jsonStr)
-	fmt.Printf("MESSAGE is:\n%s\n", jsonStr)
 
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
+	client := &http.Client{Timeout: 5 * time.Second}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		fmt.Printf("[ FATAL ERROR ] %s\n", err)
+		os.Exit(1)
 	}
+
 	defer resp.Body.Close()
 
 	fmt.Println("Result:", resp.Status)
